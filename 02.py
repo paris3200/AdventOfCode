29748db439322ef7201908520ec5df911b0d3b49
Jason Paris <paris3200@gmail.com>
2019-12-05 16:40:48 -0500

    Refactor day 2

--- 02.py
+++ 02.py
@@ -1,3 +1,6 @@
+import utils
+
+
 class Computer:
     def __init__(self, intcode):
         self.intcode = list(intcode)
@@ -6,9 +9,14 @@ class Computer:
         opcode = self.intcode[index]
 
         if opcode != 99:
-            value1 = self.intcode[self.intcode[index + 1]]
-            value2 = self.intcode[self.intcode[(index + 2)]]
-            storage = self.intcode[index + 3]
+            try:
+                value1 = self.intcode[self.intcode[index + 1]]
+                value2 = self.intcode[self.intcode[(index + 2)]]
+                storage = self.intcode[index + 3]
+            except IndexError as error:
+                raise SystemExit(
+                    "The program reached the end of the instructions without encountering a stop code. Please check the input."
+                )
 
             if opcode == 1:
                 self.intcode[storage] = value1 + value2
@@ -18,6 +26,39 @@ class Computer:
                 self.process(index + 4)
 
         return self.intcode[0]
+
+    def calculate_solution(self, target):
+        CODE = self.intcode
+        for noun in range(0, 100):
+            for verb in range(0, 100):
+                code = CODE.copy()
+
+                code[1] = noun
+                code[2] = verb
+                self.intcode = code
+                solution = self.process()
+                if self.process() == target:
+                    return [noun, verb]
+
+
+def part_one():
+    code = utils.read_file("02_input.txt", "list")
+
+    # Reset to 1202 Program State
+    code[1] = 12
+    code[2] = 2
+    wopr = Computer(code)
+    print(f"Part One: {wopr.process()}")
+
+
+def part_two():
+    code = utils.read_file("02_input.txt", "list")
+
+    wopr = Computer(code)
+    inputs = wopr.calculate_solution(19690720)
+    noun = inputs[0]
+    verb = inputs[1]
+    print(f"Part two: {100 * noun + verb}")
 
 
 def test_calibration():
@@ -29,15 +70,6 @@ def test_calibration():
 
 
 if __name__ == "__main__":
-    with open("02_input.txt", "r") as procedure:
-        for line in procedure:
-            data = line.strip().split(",")
-            code = []
-            for num in data:
-                code.append(int(num))
 
-    # Reset to 1202 Program State
-    code[1] = 12
-    code[2] = 2
-    wopr = Computer(code)
-    print(wopr.process())
+    part_one()
+    part_two()
