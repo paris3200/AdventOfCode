504d551c01881d766cb6a864d27681184b122d87
Jason Paris <paris3200@gmail.com>
2021-08-10 14:31:54 -0400

    Setup day 6

---
+++ code/2020/06.py
@@ -0,0 +1,19 @@
+import utils
+
+
+def part_one():
+    print(f"Part One")
+
+
+def part_two():
+    print(f"Part Two")
+
+
+def test_if_it_works():
+    assert True is True
+
+
+if __name__ == "__main__":
+
+    part_one()
+    part_two()
