b99b68a79b54831afbdaccf00b05154648955987
Jason Paris <paris3200@gmail.com>
2021-08-11 21:02:35 -0400

    Clean up template

--- code/2020/template.py
+++ code/2020/template.py
@@ -2,18 +2,24 @@ import utils
 
 
 def part_one():
-    print(f"Part One")
+    print("Part One")
 
 
 def part_two():
-    print(f"Part Two")
+    print("Part Two")
 
 
-def test_if_it_works():
-    assert True is True
+def test_part_one():
+    result = part_one(data)
+    assert result is True
+
+
+def test_part_two(data):
+    result = part_two(data)
+    assert result is True
 
 
 if __name__ == "__main__":
-
-    part_one()
-    part_two()
+    data = "data/xx.data"
+    part_one(data)
+    part_two(data)
