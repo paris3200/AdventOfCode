3cf9d704caf632eaf51d977a417a9ba93005711d
Jason Paris <paris3200@gmail.com>
2021-08-16 07:23:49 -0400

    Solved part 2

--- code/2020/08.py
+++ code/2020/08.py
@@ -2,14 +2,34 @@ import utils
 
 
 class Compiler:
-    def __init__(self, program):
+    def __init__(self, program, bootable=False):
         self.accumaltor = 0
         self.current_index = 0
         self.commands_run = []
         self.program = program
+        self.bootable = bootable
+
+        if self.bootable:
+            self.original_program = program.copy()
+            self.commands_changed = []
+            self.booted = False
+            self.changed = False
 
     def excute_program(self):
-        return self.execute_instruction(self.parse_instruction(self.program[0]))
+        result = self.execute_instruction(self.parse_instruction(self.program[0]))
+        if result == "False":
+            self.program = self.original_program.copy()
+            self.booted = False
+            self.changed = False
+            self.commands_run = []
+            self.accumaltor = 0
+            self.current_index = 0
+
+            return self.excute_program()
+        else:
+            return result
+
+
 
     def parse_instruction(self, instruction):
         split = instruction.split()
@@ -20,24 +40,77 @@ class Compiler:
 
     def execute_instruction(self, instruction):
 
-        if self.current_index not in self.commands_run:
-            self.commands_run.append(self.current_index)
-        else:
-            return self.accumaltor
+        if not self.bootable:
 
-        if instruction["type"] == "acc":
-            self.accumaltor += instruction["argument"]
-            self.current_index += 1
+            if self.current_index not in self.commands_run:
+                self.commands_run.append(self.current_index)
+            else:
+                return self.accumaltor
 
-        elif instruction["type"] == "jmp":
-            self.current_index += instruction["argument"]
+            if instruction["type"] == "acc":
+                self.accumaltor += instruction["argument"]
+                self.current_index += 1
 
-        elif instruction["type"] == "nop":
-            self.current_index += 1
+            elif instruction["type"] == "jmp":
+                self.current_index += instruction["argument"]
 
-        return self.execute_instruction(
-            self.parse_instruction(self.program[self.current_index])
-        )
+            elif instruction["type"] == "nop":
+                self.current_index += 1
+
+            return self.execute_instruction(
+                self.parse_instruction(self.program[self.current_index])
+            )
+
+        if self.bootable:
+
+            if self.current_index in self.commands_run:
+                # The incorrect command was changed reset and rerun
+                return "False"
+
+            if instruction["type"] == "acc":
+
+                self.accumaltor += instruction["argument"]
+                self.commands_run.append(self.current_index)
+                self.current_index += 1
+
+            elif instruction["type"] == "jmp":
+                if (
+                    self.changed is False
+                    and self.current_index not in self.commands_changed
+                ):
+                    # Change command to nop and rerun index
+                    self.changed = True
+                    self.program[self.current_index] = f"nop {instruction['argument']}"
+                    self.commands_changed.append(self.current_index)
+
+                else:
+
+                    self.commands_run.append(self.current_index)
+                    self.current_index += instruction["argument"]
+
+            elif instruction["type"] == "nop":
+
+                if (
+                    self.changed is False
+                    and self.current_index not in self.commands_changed
+                    and (self.current_index != 0 and instruction["argument"] == 0)
+                ):
+                    # Change command to jmp and rerun index
+                    self.changed = True
+                    self.program[self.current_index] = f"jmp {instruction['argument']}"
+                    self.commands_changed.append(self.current_index)
+
+                else:
+                    self.commands_run.append(self.current_index)
+                    self.current_index += 1
+
+            try:
+                return self.execute_instruction(
+                    self.parse_instruction(self.program[self.current_index])
+                )
+            except IndexError:
+                return self.accumaltor
+
 
 
 def part_one(data):
@@ -49,6 +122,9 @@ def part_one(data):
 
 def part_two(data):
     print("Part Two")
+    program = utils.read_lines(data)
+    compiler = Compiler(program, bootable=True)
+    return compiler.excute_program()
 
 
 def test_part_one():
@@ -56,12 +132,21 @@ def test_part_one():
     result = part_one(data)
     assert result == 5
 
+def test_part_one_real_data():
+    data = "data/08.data"
+    result = part_one(data)
+    assert result == 1584
 
 def test_part_two():
     data = "data/08_tests.data"
     result = part_two(data)
-    assert result is True
+    assert result == 8
 
+
+def test_part_two_real_data():
+    data = "data/08.data"
+    result = part_two(data)
+    assert result == 920
 
 def test_parse_instructions():
     program = utils.read_lines("data/08_tests.data")
@@ -75,4 +160,4 @@ def test_parse_instructions():
 if __name__ == "__main__":
     data = "data/08.data"
     print(part_one(data))
-    part_two(data)
+    print(part_two(data))
