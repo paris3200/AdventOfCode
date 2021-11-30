59394374ee23d05360d0d9da2808d53d566c9ace
Jason Paris <paris3200@gmail.com>
2021-11-30 18:33:06 -0500

    Setup 2021

---
+++ code/2021/template.py
@@ -0,0 +1,25 @@
+import utils
+
+
+def part_one():
+    print("Part One")
+
+
+def part_two():
+    print("Part Two")
+
+
+def test_part_one():
+    result = part_one(data)
+    assert result is True
+
+
+def test_part_two(data):
+    result = part_two(data)
+    assert result is True
+
+
+if __name__ == "__main__":
+    data = "data/xx.data"
+    part_one(data)
+    part_two(data)
