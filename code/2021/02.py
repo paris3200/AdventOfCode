f7076d84e84d8f4db29d7d6c7447b328488a517e
Jason Paris <paris3200@gmail.com>
2021-12-02 14:02:56 -0500

    Solve D2 P2

--- code/2021/02.py
+++ code/2021/02.py
@@ -6,6 +6,7 @@ class Submarine():
     def __init__(self):
         self.xvalue = 0
         self.yvalue = 0
+        self.aim = 0
 
     def move(self, command):
         command = command.split()
@@ -17,6 +18,17 @@ class Submarine():
         elif command[0] == "up":
             self.yvalue -= int(command[1])
 
+    def navigate(self, command):
+        command = command.split()
+
+        if command[0] == "forward":
+            self.xvalue += int(command[1])
+            self.yvalue += int(command[1]) * self.aim
+        elif command[0] == "down":
+            self.aim += int(command[1])
+        elif command[0] == "up":
+            self.aim -= int(command[1])
+
 
 def part_one(data):
     sub = Submarine()
@@ -25,7 +37,10 @@ def part_one(data):
     return (sub.xvalue * sub.yvalue)
 
 def part_two(data):
-    print("Part Two")
+    sub = Submarine()
+    for command in utils.read_lines(data):
+        sub.navigate(command)
+    return (sub.xvalue * sub.yvalue)
 
 
 def test_part_one():
@@ -37,11 +52,12 @@ def test_part_one():
 def test_part_two():
     data = "data/02_test.data"
     result = part_two(data)
-    assert result is True
+    assert result == 900
 
 
 if __name__ == "__main__":
     data = "data/02.data"
     print("Part One")
     print(part_one(data))
-    part_two(data)
+    print("Part One")
+    print(part_two(data))
