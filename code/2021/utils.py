2cbd031c0256c55ec849a584ec944af115d93d41
Jason Paris <paris3200@gmail.com>
2021-12-04 20:20:35 -0500

    Add support for binary strings.

--- code/2021/utils.py
+++ code/2021/utils.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def read_file(filename, datatype):
+def read_file(filename, datatype, int=True):
     with open(filename, "r") as procedure:
 
         if datatype == "list":
             code = []
             for line in procedure:
                 data = line.strip().split(",")
+
                 for num in data:
-                    code.append(int(num))
+                    if int:
+                        code.append(int(num))
+                    else:
+                        code.append(num)
 
             return code
 
