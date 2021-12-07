c9e3b526e3c842b5a7239c4f4731d6b97aa3e08e
Jason Paris <paris3200@gmail.com>
2021-12-07 08:32:11 -0500

    Refactor create_line()

--- code/2021/05/aoc_05.py
+++ code/2021/05/aoc_05.py
@@ -13,13 +13,13 @@ def create_line(start, stop):
     # Horizontal Lines
     if start[0] == stop[0]:
         delta = stop[1] - start[1]
-        for  x, y in enumerate(range(start[1], delta+1)):
+        for y in range(start[1], delta + 1):
             line.append([start[0], y])
         line.append(stop)
 
     # Vertical Lines
     elif start[1] == stop[1]:
-        for  x, y in enumerate(range(stop[0], start[0])):
+        for y in range(stop[0], start[0]):
             line.append([y, start[1]])
         line.append(start)
         line.reverse()
