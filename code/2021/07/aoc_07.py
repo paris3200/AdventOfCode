d3771a0db87a0cd87d3e118ce6f62a4883690bf1
Jason Paris <paris3200@gmail.com>
2021-12-10 16:11:48 -0500

    Solve Part 2

--- code/2021/07/aoc_07.py
+++ code/2021/07/aoc_07.py
@@ -4,14 +4,87 @@ import utils
 
 
 def calculate_distance(p1: int, p2: int) -> int:
+    """Calculate the absolute value between two points.
+
+    Args:
+        p1: Point 1
+        p2: Point 2
+
+    Returns:
+        int:  distance between p1 and p2
+    """
     return abs(p1 - p2)
 
 
-def move_subs(subs: List[int], point: int) -> int:
+def calculate_fuel_table(subs: List[int]) -> List[int]:
+    """Calculate fuel table where the index represents the distance traveled
+    and the value represents the fuel used to dravel that distance.
+
+    Args:
+        subs: List containing the horizontal location of each sub.
+
+    Returns:
+        list: fuel table
+    """
+    fuel_table = [0]
+    for x in range(1, max(subs) + 1):
+        fuel_table.append(x + fuel_table[x - 1])
+
+    return fuel_table
+
+
+def move_subs(subs: List[int], point: int, fuel_table: List[int] = None) -> int:
+    """Moves all the subs and to the point and determines the fuel required.
+
+    Args:
+        subs:  List contain the horizontal position of each sub.
+        point:  The position to move all the subs to.
+        fuel_table: List containing the fuel rates.
+    Returns:
+        int:  Fuel used to move all subs to point.
+    """
     fuel_used = 0
-    for sub in subs:
-        fuel_used += calculate_distance(sub, point)
+
+    if not fuel_table:
+        for sub in subs:
+            fuel_used += calculate_distance(sub, point)
+    else:
+        for sub in subs:
+            fuel_used += fuel_table[calculate_distance(sub, point)]
     return fuel_used
+
+
+def calculate_central_point(subs: List[int], fuel_rate: bool = False) -> int:
+    """Calculates which point all subs can be moved to using the
+    least ammount of fuel possible.
+
+    Args:
+        subs:  List containing initial position of all subs.
+        fuel_rate:  True if fuel rate needed.
+
+    Returns:
+        int: Point that uses the least amount of fuel to move all subs to.
+    """
+    fuel_used = 2147483647
+    point = 0
+    points = []
+    fuel_table = calculate_fuel_table(subs)
+
+    # There has to be a better way
+    for x in range(min(subs), max(subs)):
+        if x > 0:
+            points.append(x)
+
+    for pt in points:
+        if fuel_rate:
+            fuel = move_subs(subs, pt, fuel_table)
+        else:
+            fuel = move_subs(subs, pt)
+        if fuel < fuel_used:
+            fuel_used = fuel
+            point = pt
+
+    return point
 
 
 def part_one(subs: List[int]) -> int:
@@ -20,27 +93,11 @@ def part_one(subs: List[int]) -> int:
     return fuel
 
 
-def calculate_central_point(subs: List[int]) -> int:
-    fuel_used = 2147483647
-    point = 0
-    points = []
-
-    # There has to be a better way
-    for x in range(min(subs), max(subs)):
-        if x > 0:
-            points.append(x)
-
-    for pt in points:
-        fuel = move_subs(subs, pt)
-        if fuel < fuel_used:
-            fuel_used = fuel
-            point = pt
-
-    return point
-
-
-def part_two():
-    pass
+def part_two(subs: List[int]) -> int:
+    fuel_table = calculate_fuel_table(subs)
+    point = calculate_central_point(subs, fuel_rate=True)
+    fuel = move_subs(subs, point, fuel_table)
+    return fuel
 
 
 if __name__ == "__main__":
@@ -52,5 +109,7 @@ if __name__ == "__main__":
     print("Part One")
     print(f"All subs moved to {point} with a total fuel consumption of {fuel}")
 
+    point = calculate_central_point(subs, fuel_rate=True)
+    fuel = part_two(subs)
     print("Part Two")
-    part_two()
+    print(f"All subs moved to {point} with a total fuel consumption of {fuel}")
--- code/2021/07/tests.py
+++ code/2021/07/tests.py
@@ -1,6 +1,12 @@
 import utils
-from aoc_07 import (calculate_central_point, calculate_distance, move_subs,
-                    part_one)
+from aoc_07 import (
+    calculate_central_point,
+    calculate_distance,
+    move_subs,
+    part_one,
+    part_two,
+    calculate_fuel_table,
+)
 
 
 def test_calculate_distance_between_points_returns_distance():
@@ -22,18 +28,37 @@ def test_move_subs_returns_fuel_used():
     assert result == 37
 
 
-def test_part_one_with_test_data():
-    subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
-    result = part_one(subs)
-
-    assert result == 37
-
-
 def test_calculate_central_point():
     subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
     result = calculate_central_point(subs)
 
     assert result == 2
+
+
+def test_calculate_fuel_table_large_move():
+    subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    result = calculate_fuel_table(subs)
+    expected = [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91, 105, 120, 136]
+
+    assert result == expected
+
+
+def test_move_subs_returns_fuel_used_with_fuel_table():
+    subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    fuel_table = calculate_fuel_table(subs)
+    result = move_subs([16], 5, fuel_table)
+
+    assert result == 66
+
+    result = move_subs([14], 5, fuel_table)
+    assert result == 45
+
+
+def test_part_one_with_test_data():
+    subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    result = part_one(subs)
+
+    assert result == 37
 
 
 def test_part_one_with_problem_data():
@@ -42,3 +67,20 @@ def test_part_one_with_problem_data():
     result = part_one(subs)
 
     assert result == 341534
+
+
+def test_part_two_with_test_data():
+    subs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+    result = part_two(subs)
+    point = calculate_central_point(subs, fuel_rate=True)
+    assert point == 5
+    assert result == 168
+
+
+def test_part_two_with_problem_data():
+    filename = "data/07.data"
+    subs = utils.read_file_of_ints(filename)
+    result = part_two(subs)
+    point = calculate_central_point(subs, fuel_rate=True)
+    assert point == 484
+    assert result == 93397632
