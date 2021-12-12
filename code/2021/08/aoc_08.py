174f17cd234917eb14f3774a3276adeb1208eaec
Jason Paris <paris3200@gmail.com>
2021-12-12 09:05:56 -0500

    Solve Part 1

--- code/2021/08/aoc_08.py
+++ code/2021/08/aoc_08.py
@@ -1,6 +1,79 @@
+from typing import Optional, List
 
-def part_one():
-    pass
+
+display_patterns = []
+display_patterns.append({"a", "b", "c", "e", "f", "g"})
+display_patterns.append({"c", "f"})
+display_patterns.append({"a", "c", "d", "e", "g"})
+display_patterns.append({"a", "c", "d", "f", "g"})
+display_patterns.append({"b", "c", "d", "f"})
+display_patterns.append({"a", "b", "d", "f", "g"})
+display_patterns.append({"a", "b", "d", "e", "f", "g"})
+display_patterns.append({"a", "c", "f"})
+display_patterns.append({"a", "b", "c", "d", "e", "f", "g"})
+display_patterns.append({"a", "b", "c", "d", "f", "g"})
+
+
+def get_one(input: list) -> Optional[str]:
+    for segment in input:
+        if len(segment) == 2:
+            return segment
+    return None
+
+
+def get_four(input: list) -> Optional[str]:
+    for segment in input:
+        if len(segment) == 4:
+            return segment
+    return None
+
+
+def get_seven(input: list) -> Optional[str]:
+    for segment in input:
+        if len(segment) == 3:
+            return segment
+    return None
+
+
+def get_eight(input: list) -> Optional[str]:
+    for segment in input:
+        if len(segment) == 7:
+            return segment
+    return None
+
+
+def read_file(filename) -> List[str]:
+    with open(filename, "r") as contents:
+        readings = []
+        for line in contents:
+            data = line.strip()
+            data = data.split(" | ")
+            observations = data[0].split()
+            output = data[1].split()
+            readings.append([observations, output])
+
+    return readings
+
+
+def get_occurances(data) -> int:
+    occurances = 0
+
+    for line in data:
+        for observation in line[1]:
+            if (
+                len(observation) == 3
+                or len(observation) == 7
+                or len(observation) == 4
+                or len(observation) == 2
+            ):
+                occurances += 1
+
+    return occurances
+
+
+def part_one(filename: str) -> int:
+    readings = read_file(filename)
+    return get_occurances(readings)
 
 
 def part_two():
@@ -8,8 +81,10 @@ def part_two():
 
 
 if __name__ == "__main__":
+    filename = "data/08.data"
     print("Part One")
-    part_one()
+    count = part_one(filename)
+    print(f"In the output values, 1, 4, 7, 8 appear:  {count} times")
 
     print("Part Two")
     part_two()
---
+++ code/2021/08/tests.py
@@ -0,0 +1,97 @@
+from aoc_08 import (
+    display_patterns,
+    get_four,
+    get_one,
+    get_seven,
+    get_eight,
+    read_file,
+    part_one,
+    part_two,
+    get_occurances,
+)
+
+
+def test_set0_has_6_characters():
+    assert len(display_patterns[0]) == 6
+
+
+def test_set1_has_2_characters():
+    assert len(display_patterns[1]) == 2
+
+
+def test_set2_has_5_characters():
+    assert len(display_patterns[2]) == 5
+
+
+def test_set3_has_5_characters():
+    assert len(display_patterns[3]) == 5
+
+
+def test_set4_has_4_characters():
+    assert len(display_patterns[4]) == 4
+
+
+def test_set5_has_5_characters():
+    assert len(display_patterns[5]) == 5
+
+
+def test_set6_has_6_characters():
+    assert len(display_patterns[6]) == 6
+
+
+def test_set7_has_3_characters():
+    assert len(display_patterns[7]) == 3
+
+
+def test_set8_has_7_characters():
+    assert len(display_patterns[8]) == 7
+
+
+def test_set9_has_6_characters():
+    assert len(display_patterns[9]) == 6
+
+
+def test_get_four():
+    input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split()
+    result = get_four(input)
+
+    assert result == "cgeb"
+
+
+def test_get_one():
+    input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split()
+    result = get_one(input)
+
+    assert result == "be"
+
+
+def test_get_seven():
+    input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split()
+    result = get_seven(input)
+
+    assert result == "edb"
+
+
+def test_get_eight():
+    input = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split()
+    result = get_eight(input)
+
+    assert result == "cfbegad"
+
+
+def test_read_file():
+    data = "data/08_test.data"
+    result = read_file(data)
+
+    observations = "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb".split()
+    readings = "fdgacbe cefdb cefbgd gcbe".split()
+
+    assert result[0][0] == observations
+    assert result[0][1] == readings
+
+
+def test_part_one_with_test_data():
+    data = "data/08_test.data"
+    result = part_one(data)
+
+    assert result == 26
