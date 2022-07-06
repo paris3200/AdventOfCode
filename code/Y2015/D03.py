a678a4846f2c3c2fb625c340c7b8844d27e5bd7b
Jason Paris <paris3200@gmail.com>
2022-07-06 13:28:30 -0400

    Solve D3 P2

--- code/Y2015/D03.py
+++ code/Y2015/D03.py
@@ -1,62 +1,102 @@
 if __name__ != "__main__":
     from Y2015.utils import Grid
-    import utils
+    from Y2015 import utils
 else:
     from utils import Grid
     import utils
+
+input = utils.read_lines("data/03.data")
+DATA = input[0]
+
 
 def grid_size(directions: str) -> list:
     """Returns the max gride size"""
     y = directions.count("^") + directions.count("v")
     x = directions.count(">") + directions.count("<")
-    if x < 5:
+    if x <= 5:
         x = 5
-    if y < 5:
+    if y <= 5:
         y = 5
     return [x, y]
 
 
-def deliver_presents(nav_list: str) -> Grid:
+def process_instruction(instruction: str, start: list) -> list:
+    if instruction == "^":
+        start[1] += 1
+    elif instruction == "v":
+        start[1] -= 1
+    elif instruction == ">":
+        start[0] += 1
+    elif instruction == "<":
+        start[0] -= 1
+
+    return start
+
+
+def create_grid(x: int, y: int) -> list[Grid, int, int]:
+    index = [int(x / 2), int(y / 2)]
+    grid = Grid(x, y)
+    return [grid, index[0], index[1]]
+
+
+def deliver_presents(nav_list: str, grid: Grid, index_x: int, index_y: int) -> Grid:
     """Increases the present count at the grid location based on nav_list locations."""
-    size = grid_size(nav_list)
-    index = [int(size[0] / 2), int(size[1] / 2)]
-    current_index = index
-    grid = Grid(size[0], size[1])
 
     # Deliver present to start point
-    grid.increase_point(current_index[0], current_index[1])
+    grid.increase_point(index_x, index_y)
 
     directions = list(nav_list)
+    current_index = [index_x, index_y]
     for instruction in directions:
-        if instruction == "^":
-            current_index[1] += 1
-        elif instruction == "v":
-            current_index[1] -= 1
-        elif instruction == ">":
-            current_index[0] += 1
-        elif instruction == "<":
-            current_index[0] -= 1
-
+        current_index = process_instruction(instruction, current_index)
         grid.increase_point(current_index[0], current_index[1])
 
     return grid
 
 
-def part_one(data):
-    grid = deliver_presents(data)
+def part_one(data=DATA):
+    directions = data
+    size = grid_size(directions)
+    grid_properties = create_grid(size[0], size[1])
+    grid = deliver_presents(
+        directions, grid_properties[0], grid_properties[1], grid_properties[2]
+    )
 
     stats = grid.stats()
-    return stats['on']
+    return stats["on"]
 
 
-def part_two(data):
-    pass
+def part_two(data=DATA):
+    directions = data
+    santa_directions = str(directions[::2])
+    robo_directions = str(directions[1::2])
+
+    santa_size = grid_size(santa_directions)
+    robot_size = grid_size(robo_directions)
+
+    if santa_size[0] > robot_size[0]:
+        x = santa_size[0]
+    else:
+        x = robot_size[0]
+
+    if santa_size[1] > robot_size[1]:
+        y = santa_size[1]
+    else:
+        y = robot_size[1]
+
+    grid_properties = create_grid(x, y)
+    santa_grid = deliver_presents(
+        santa_directions, grid_properties[0], grid_properties[1], grid_properties[2]
+    )
+    grid = deliver_presents(
+        robo_directions, santa_grid, grid_properties[1], grid_properties[2]
+    )
+    stats = grid.stats()
+    return stats["on"]
 
 
 if __name__ == "__main__":
-    data = "data/03.data"
-    input = utils.read_lines(data)
     print("Part One")
-    print(part_one(input[0]))
+    print(part_one())
     print("Part Two")
-    print(part_two(input))
+    print(part_two())
--- code/Y2015/tests/test_d03.py
+++ code/Y2015/tests/test_d03.py
@@ -7,35 +7,23 @@ def test_grid_size_creates_minimum():
     assert [5, 5] == D03.grid_size(">")
 
 
-def test_deliver_presents():
-    grid = D03.deliver_presents(">")
-
-    assert grid.grid == [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 1, 1, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-    ]
-
-
-def test_deliver_presents_delivers_correct_amount():
-    grid = D03.deliver_presents(">")
-    stats = grid.stats()
-    assert stats['on'] == 2
-
-    grid = D03.deliver_presents("^v^v^v^v^v")
-    stats = grid.stats()
-    assert stats['on'] == 2
-
-
 @pytest.mark.skip()
-def test_part_one():
-    result = D03.part_one(">")
-    assert result is True
+def test_part_one_with_sample_data():
+    presents = D03.part_one(">")
+    assert presents == 2
+
+    presents = D03.part_one("^v^v^v^v^v")
+    assert presents == 2
+
+def test_part_one_correct_answer():
+    presents = D03.part_one()
+    assert presents == 2592
 
 
-@pytest.mark.skip()
-def test_part_two():
-    result = D03.part_two(">")
-    assert result is True
+def test_part_two_correct_answer():
+    presents = D03.part_two('^v')
+    assert presents == 3
+
+    presents = D03.part_two('^>v<')
+    assert presents == 3
+
