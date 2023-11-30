9b87a07c2556a798820dbff6fcf050204b43b31b
Jason Paris <paris3200@gmail.com>
2023-11-30 15:43:24 -0500

    Solve Part 2

--- code/2017/D04/aoc_04.py
+++ code/2017/D04/aoc_04.py
@@ -18,6 +18,18 @@ def read_lines(filename: str) -> list[str]:
     return data
 
 
+def is_anagram(passphrase: str) -> bool:
+    words = passphrase.split(" ")
+    for word_one in words:
+        remaining_words = words.copy()
+        remaining_words.remove(word_one)
+
+        for word_two in remaining_words:
+            if sorted(word_one) == sorted(word_two):
+                return True
+    return False
+
+
 def part_one(filename: str) -> int:
     lines = read_lines(filename)
     counter = 0
@@ -28,8 +40,14 @@ def part_one(filename: str) -> int:
     return counter
 
 
-def part_two():
-    pass
+def part_two(filename: str) -> int:
+    lines = read_lines(filename)
+    counter = 0
+    for line in lines:
+        if is_anagram(line) is False:
+            counter += 1
+
+    return counter
 
 
 if __name__ == "__main__":
@@ -37,4 +55,4 @@ if __name__ == "__main__":
     print(part_one("input"))
 
     print("Part Two")
-    part_two()
+    print(part_two("input"))
--- code/2017/D04/tests.py
+++ code/2017/D04/tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from aoc_04 import validate_passphrase
+from aoc_04 import validate_passphrase, is_anagram
 
 
 @pytest.mark.parametrize(
@@ -13,3 +13,17 @@ from aoc_04 import validate_passphrase
 )
 def test_validate_passphrase(passphrase, is_valid) -> None:
     assert validate_passphrase(passphrase) == is_valid
+
+
+@pytest.mark.parametrize(
+    ("passphrase", "is_valid"),
+    (
+        ("abcde fghij", False),
+        ("abcde xyz ecdab", True),
+        ("a ab abc abd abf abj", False),
+        ("iiii oiii ooii oooi oooo", False),
+        ("oiii ioii iioi iiio", True),
+    ),
+)
+def test_is_anagram(passphrase, is_valid) -> None:
+    assert is_anagram(passphrase) == is_valid
