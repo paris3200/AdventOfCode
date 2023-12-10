70629c249b3f8d974679e875ce04a57753a4adc7
Jason Paris <paris3200@gmail.com>
2023-12-09 19:11:12 -0500

    WIP Tests pass

--- code/2023/D09/aoc_09.py
+++ code/2023/D09/aoc_09.py
@@ -13,47 +13,88 @@ def read_lines(filename: str) -> list[str]:
 
 
 def get_node_difference(node1: int, node2: int) -> int:
-    return abs(node2 - node1)
+    return node2 - node1
+
+
+def solve_missing(num1: int, result: int) -> int:
+    return result + num1
 
 
 def reduce_sequence(sequence: list[int]) -> list[int]:
     solved_sequence = []
     for x, y in pairwise(sequence):
         solved_sequence.append(get_node_difference(x, y))
-
     return solved_sequence
 
 
-def solve_sequence(sequence: list[list[int]]) -> list[list[int]]:
-    while set(sequence[-1]) != {0}:
-        sequence.append(reduce_sequence(sequence[-1]))
+def solve_sequence(sequence: list[int]) -> list[list[int]]:
+    solved_sequence = [sequence]
+    while True:
+        reduced_sequence = reduce_sequence(solved_sequence[-1])
+        solved_sequence.append(reduced_sequence)
 
-    return sequence
+        if set(solved_sequence[-1]) == {0}:
+            return solved_sequence
+
+
+def solve_sequence_up(sequences: list[list[int]]) -> list[list[int]]:
+    end_index = len(sequences) - 1
+    sequences[end_index].append(0)
+    for index in range(end_index, -1, -1):
+        if index != end_index:
+            solution = solve_missing(sequences[index + 1][-1], sequences[index][-1])
+            sequences[index].append(solution)
+
+    return sequences
 
 
 def print_sequence(sequences: list[list[int]]) -> None:
     for index, sequence in enumerate(sequences):
         list_str = map(str, sequence)
-        line = ' '.join(list(list_str))
-        shift = ' ' * index
+        line = " ".join(list(list_str))
+        shift = " " * index
         print(shift + line)
     print(" ")
+
+
+def write_sequence(sequences: list[list[int]]) -> None:
+    with open("discovery_text", "a") as file:
+        for index, sequence in enumerate(sequences):
+            list_str = map(str, sequence)
+            line = " ".join(list_str)
+            shift = " " * index
+            file.write(shift + line)
+            file.write("\n")
+        file.write("\n")
 
 
 def process_input(lines: list[str]) -> None:
     sequences = []
     for line in lines:
-        line = line.split(" ")
-        sequences.append(list(map(int, line)))
+        num = line.split(" ")
+        sequences.append(list(map(int, num)))
     return sequences
 
 
 def part_one(filename: str):
     sequences = process_input(read_lines(filename))
-
+    solved_sequences = []
     for sequence in sequences:
-        print_sequence(solve_sequence([sequence]))
-        break
+        solution = solve_sequence(sequence)
+        # write_sequence(solution)
+        solved_sequences.append(solution)
+
+    final_sequences = []
+    for sequence in solved_sequences:
+        solution = solve_sequence_up(sequence)
+        # write_sequence(solution)
+        final_sequences.append(solution)
+
+    values = []
+    for sequence in final_sequences:
+        values.append(sequence[0][-1])
+
+    return sum(values)
 
 
 def part_two(filename: str):
@@ -62,7 +103,7 @@ def part_two(filename: str):
 
 if __name__ == "__main__":
     print("Part One")
-    print(part_one("test_input"))
+    print(part_one("input"))
 
     print("Part Two")
     print(part_two("input"))
--- code/2023/D09/tests.py
+++ code/2023/D09/tests.py
@@ -1,4 +1,4 @@
-from aoc_09 import  get_node_difference, reduce_sequence
+from aoc_09 import get_node_difference, reduce_sequence, part_one, solve_missing
 
 
 def test_node_difference() -> None:
@@ -7,11 +7,19 @@ def test_node_difference() -> None:
 
 def test_node_difference_with_negative_result() -> None:
     assert get_node_difference(0, 2) == 2
+    assert get_node_difference(-101, -96) == 5
 
+
+def test_solve_missting() -> None:
+    assert solve_missing(-5, -101) == -106
 
 
 def test_reduce_sequence() -> None:
     input_sequence = [0, 3, 6, 9, 12, 15]
     result_sequence = [3, 3, 3, 3, 3]
-
     assert reduce_sequence(input_sequence) == result_sequence
+
+
+def test_part_one_test_input() -> None:
+    assert part_one("test_input") == 114
+    assert part_one("input") > 1679257426  # Too Low
